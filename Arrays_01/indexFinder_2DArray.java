2a4ab15977e44142f1dabeacee44d53ec7487a84
Ali Hasan Mubarak <royalihasan0987@gmail.com>
2022-04-14 12:44:30 +0500

    "2D Array problem"

---
+++ Arrays_01/indexFinder_2DArray.java
@@ -0,0 +1,33 @@
+package Arrays_01;
+
+import java.util.Scanner;
+
+public class indexFinder_2DArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows");
+        int row = sc.nextInt();
+        System.out.println("Enter columns");
+        int cols = sc.nextInt();
+        int[][] numbers = new int[row][cols];
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        // KEY INPUT
+        System.out.println("Enter your key here");
+        int key = sc.nextInt();
+        // Iterate
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (key == numbers[i][j]) {
+                    System.out.println("(x,y)=(" + i + "," + j + ")" + " ");
+                }
+            }
+        }
+        System.out.println();
+    }
+}
